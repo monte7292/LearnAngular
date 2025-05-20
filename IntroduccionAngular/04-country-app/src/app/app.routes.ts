c2f3cceb1de9284eb6485424375ae4f20125426b
monte <alejandromonterodeltoro@gmail.com>
2025-05-20 13:55:29 +0200

    Pagina Básica botones Angular TailWind v2

--- IntroduccionAngular/04-country-app/src/app/app.routes.ts
+++ IntroduccionAngular/04-country-app/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+   {
+    //Esto es un lazy loading que baja la carga de los componentes de country.routes.ts
+    //En vez de cargarlos todos al inicio, se cargan cuando se necesitan
+    path: 'country',
+    loadChildren: () => import('./country/country.routes')
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  }
+
+
+];
