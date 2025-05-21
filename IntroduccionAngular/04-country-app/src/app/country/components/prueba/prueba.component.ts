c75404a2c42178369a4fdcaf56631899b2b512a9
monte <alejandromonterodeltoro@gmail.com>
2025-05-21 13:34:10 +0200

    Cambios guardados

--- IntroduccionAngular/04-country-app/src/app/country/components/prueba/prueba.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-prueba',
-  imports: [],
-  templateUrl: './prueba.component.html',
-})
-export class PruebaComponent { }
